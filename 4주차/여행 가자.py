435014708ccf7f13382c7422e69d2e64cc1e324f
SangwonYoon <yoonsw0532@naver.com>
2021-08-18 14:04:18 +0900

    4th week

--- 4주차/여행 가자.py
+++ 4주차/여행 가자.py
@@ -1,1 +1,25 @@
- 
+def visitable(city):
+    for i in range(N):
+        if conn[city][i] and visit[i] == 0:
+            visit[i] = 1
+            visitable(i)
+
+N = int(input())
+M = int(input())
+
+conn = list()
+for i in range(N):
+    conn.append(list(map(int,input().split())))
+
+plan = list(map(int,input().split()))
+
+visit = [0]*N
+visit[plan[0]-1] = 1
+visitable(plan[0]-1)
+
+for i in range(M):
+    if visit[plan[i]-1] == 0:
+        print("NO")
+        break
+else:
+    print("YES") 
