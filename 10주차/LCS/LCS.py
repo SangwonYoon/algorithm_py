25023dd575260280022853dfe69464df5d3d1001
SangwonYoon <yoonsw0532@naver.com>
2021-10-08 12:05:21 +0900

    21-10-08 algorithm study

--- 10주차/LCS/LCS.py
+++ 10주차/LCS/LCS.py
@@ -1,15 +1,10 @@
 str1 = '!' + input()
 str2 = '@' + input()
-
 memo = [[0]*len(str2) for i in range(len(str1))]
 
-for i in range(len(str2)):
-    for j in range(len(str1)):
-        if not(i and j):
-            pass
-        elif str1[j] != str2[i]:
-            memo[j][i] = max(memo[j-1][i], memo[j][i-1])
-        elif str1[j] == str2[i]:
-            memo[j][i] = memo[j-1][i-1] + 1
+for i in range(1,len(str2)):
+    for j in range(1,len(str1)):
+        if str1[j] != str2[i]: memo[j][i] = max(memo[j-1][i], memo[j][i-1])
+        else: memo[j][i] = memo[j-1][i-1] + 1
 
 print(memo[len(str1)-1][len(str2)-1])
