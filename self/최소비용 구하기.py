608f2f794e363cdab799a84f92dc1dd06f1ac720
SangwonYoon <yoonsw0532@naver.com>
2022-01-15 22:08:59 +0900

    22-1-15 algorithm study

---
+++ self/최소비용 구하기.py
@@ -0,0 +1,37 @@
+#1916번
+
+import sys
+input = sys.stdin.readline
+
+def dijkstra(idx):
+    visited.append(idx)
+
+    for i in range(len(route[idx])):
+        node[route[idx][i][0]] = min(node[route[idx][i][0]], node[idx] + route[idx][i][1])
+
+    temp = node[:]
+    for i in visited:
+        temp[i] = float("inf")
+
+    target = temp.index(min(temp))
+    if target == e:
+        print(node[target])
+    else:
+        dijkstra(target)
+
+N = int(input())
+M = int(input())
+
+route = [[] for i in range(N+1)]
+
+for i in range(M):
+    a, b, cost = map(int,input().split())
+    route[a].append([b, cost])
+
+s, e = map(int,input().split())
+
+visited = []
+
+node = [float("inf") for i in range(N+1)] #s번 노드부터 각 노드까지의 최소 비용을 저장하는 리스트
+node[s] = 0
+dijkstra(s)
