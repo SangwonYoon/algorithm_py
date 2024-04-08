295f5c60095f6f20d750a0963fa2fc3ecd2a2835
sangwonYoon <yoonsw0532@naver.com>
2024-04-08 20:52:01 +0900

    2024-04-08 algorithm study

---
+++ baekjoon/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,41 @@
+# 20055번
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+durability = list(map(int, input().split()))
+robots = [0] * (N*2)
+belt = deque(map(list, zip(robots, durability)))
+
+turn = 0
+
+while True:
+    turn += 1
+
+    # phase 1
+    belt.rotate(1)
+    belt[N-1][0] = 0 # 로봇 내림
+
+    # phase 2
+    for i in range(N-2, -1, -1):
+        if belt[i][0] == 1 and belt[i+1][0] == 0 and belt[i+1][1] > 0:
+            belt[i][0] = 0
+            belt[i+1][0] = 1
+            belt[i+1][1] -= 1
+    
+    belt[N-1][0] = 0 # 로봇 내림
+
+    # phase 3
+    if belt[0][1] > 0:
+        belt[0][0] = 1
+        belt[0][1] -= 1
+
+    # phase 4
+    broken = [durab for _, durab in belt].count(0)
+    if broken >= K:
+        break
+
+print(turn)
